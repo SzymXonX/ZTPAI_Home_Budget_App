355054c8536e77b68334b0b0ca08219cc23ce669
SzymXonX <szymon.koczur5@gmail.com>
2025-04-08 16:17:13 +0200

    feat(admin): register models for Django admin interface

--- Backend/myapp/admin.py
+++ Backend/myapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from myapp.models import IncomesCategory, ExpensesCategory, Incomes, Expenses
 
-# Register your models here.
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(IncomesCategory)
+admin.site.register(ExpensesCategory)
+admin.site.register(Incomes)
+admin.site.register(Expenses)
