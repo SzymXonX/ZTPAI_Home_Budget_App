c1a01f8154d6b0161c0c65efa6edc12b3d525472
SzymXonX <szymon.koczur5@gmail.com>
2025-05-06 11:07:15 +0200

    feat(api): add user-info endpoint to retrieve user role

--- Backend/django_project/urls.py
+++ Backend/django_project/urls.py
@@ -6,12 +6,15 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
 
+from myapp.views import UserInfoView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/user/register/", CreateUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
+    path("api/user-info/", UserInfoView.as_view(), name="user_info"),
     path("api/", include("myapp.urls")),
 ]
 
