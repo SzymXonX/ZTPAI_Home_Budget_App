842f13184c6d1a784e7f6c788b784d1e8f19de4b
SzymXonX <szymon.koczur5@gmail.com>
2025-05-30 21:24:46 +0200

    feat(api): add endpoints for expenses and incomes filtered by month and year

--- Backend/myapp/urls.py
+++ Backend/myapp/urls.py
@@ -4,14 +4,18 @@ from . import views
 
 urlpatterns = [
     path("incomes/", views.IncomesView.as_view(), name="incomes"),
-    path("incomes/delete/<int:pk>/", views.IncomesDelete.as_view(), name="delete-incomes"),
-    path("incomes/categories/", views.IncomesCategoryView.as_view(), name="incomes-categories"),
-    path("incomes/categories/delete/<int:pk>/", views.IncomesCategoryDelete.as_view(), name="delete-incomes-category"),
-    
     path("expenses/", views.ExpensesView.as_view(), name="expenses"),
+
+    path("incomes/<int:year>/<int:month>/", views.IncomesView.as_view(), name="incomes-list-by-month"),
+    path("expenses/<int:year>/<int:month>/", views.ExpensesView.as_view(), name="expenses-list-by-month"),
+
+    path("incomes/delete/<int:pk>/", views.IncomesDelete.as_view(), name="delete-incomes"),
+    path("incomes/categories/delete/<int:pk>/", views.IncomesCategoryDelete.as_view(), name="delete-incomes-category"),
     path("expenses/delete/<int:pk>/", views.ExpensesDelete.as_view(), name="delete-expenses"),
-    path("expenses/categories/", views.ExpensesCategoryView.as_view(), name="expenses-categories"),
     path("expenses/categories/delete/<int:pk>/", views.ExpensesCategoryDelete.as_view(), name="delete-expenses-category"),
+    
+    path("incomes/categories/", views.IncomesCategoryView.as_view(), name="incomes-categories"),
+    path("expenses/categories/", views.ExpensesCategoryView.as_view(), name="expenses-categories"),
 
     path("summary/", views.SummaryView.as_view(), name="summary"),
     path("summary/<int:year>/<int:month>/", views.SummaryView.as_view(), name="summary_by_month"),
