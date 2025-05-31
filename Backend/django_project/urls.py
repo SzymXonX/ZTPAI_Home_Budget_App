a744bba9b69e2ac88838be47bc46f7a262207910
SzymXonX <szymon.koczur5@gmail.com>
2025-05-31 18:58:54 +0200

    feat(auth): add URL path for password change

--- Backend/django_project/urls.py
+++ Backend/django_project/urls.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
 
-from myapp.views import UserInfoView
+from myapp.views import UserInfoView, ChangePasswordView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,6 +15,7 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
     path("api/user-info/", UserInfoView.as_view(), name="user_info"),
+    path("api/user/change-password/", ChangePasswordView.as_view(), name="change_password"),
     path("api/", include("myapp.urls")),
 ]
 
