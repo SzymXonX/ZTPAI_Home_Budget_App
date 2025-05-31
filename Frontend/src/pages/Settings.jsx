3f44e38b26161cfae63fa1898032330cd20e5ff0
SzymXonX <szymon.koczur5@gmail.com>
2025-05-31 19:01:55 +0200

    feat(settings): build complete user settings page

--- Frontend/src/pages/Settings.jsx
+++ Frontend/src/pages/Settings.jsx
@@ -1,19 +1,223 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
+import api from '../api';
+
+import '../styles/Settings.css'; 
+
+import { ImEye, ImEyeBlocked } from "react-icons/im";
+
 
 function Settings() {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    const handleLogout = () => {
-        navigate('/logout');
-    };
+  const [userProfileData, setUserProfileData] = useState({
+    username: '',
+    first_name: '',
+    last_name: '',
+    email: ''
+  });
 
-    return (
-        <div>
-            Settings
-            <button onClick={handleLogout}>Logout</button>
-        </div>
-    );
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmNewPassword, setConfirmNewPassword] = useState('');
+  const [showNewPassword, setShowNewPassword] = useState(false);
+  const [showConfirmNewPassword, setShowConfirmNewPassword] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+
+	const handleLogout = () => {
+		navigate('/logout');
+	};
+
+
+	const fetchUserData = useCallback(async () => {
+    try {
+      const response = await api.get("/api/user-info/", {});
+      setUserProfileData(response.data);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+	}, []);
+
+  useEffect(() => {
+    fetchUserData();
+  }, [fetchUserData]);
+
+
+	const toggleNewPasswordVisibility = () => {
+    setShowNewPassword(prev => !prev);
+  };
+  const toggleConfirmNewPasswordVisibility = () => {
+    setShowConfirmNewPassword(prev => !prev);
+  };
+
+  const handleChangeProfileData = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const response = await api.patch('/api/user-info/', userProfileData);
+      alert("Dane profilowe zostały pomyślnie zaktualizowane.");
+      setUserProfileData(response.data); 
+    } catch (error) {
+      console.error('Błąd podczas aktualizacji danych profilowych:', error);
+      alert("Wystąpił błąd podczas aktualizacji danych profilowych. Spróbuj ponownie.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleChangePassword = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    if (!newPassword || !confirmNewPassword) {
+      alert('Wszystkie pola hasła są wymagane.');
+      setLoading(false);
+      return;
+    }
+
+    if (newPassword !== confirmNewPassword) {
+      alert('Nowe hasła nie są zgodne.');
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const passwordData = {
+          new_password: newPassword,
+          confirm_password: confirmNewPassword
+      };
+      await api.post('/api/user/change-password/', passwordData);
+      alert('Hasło zostało pomyślnie zmienione.');
+      setNewPassword('');
+      setConfirmNewPassword('');
+    } catch (error) {
+      console.error('Błąd podczas zmiany hasła:', error);
+      alert('Wystąpił błąd podczas zmiany hasła. Spróbuj ponownie.');
+    } finally {
+      setLoading(false);
+    }
+  };
+  
+	return (
+		<div className="settings-app-content">
+			<div className="dashboard-container">
+				<div className="dashboard-row">
+					<div className="settings-container">
+						<div className="settings-list" id="settings-list">
+							<form className="change-data-form" onSubmit={handleChangeProfileData}>
+                <div className='form_group'>
+                  <label htmlFor="username">nazwa użytkownika</label>
+                  <input
+                    id="username"
+                    type="text"
+                    name="username"
+                    value={userProfileData.username}
+                    onChange={(e) => setUserProfileData({ ...userProfileData, username: e.target.value })}
+                    autoComplete="off"
+                    required
+                  />
+                </div>
+								<div className='form_group'>
+									<label htmlFor="first_name">imię</label>
+									<input
+										id="first_name"
+										type="text"
+										name="first_name"
+										value={userProfileData.first_name}
+										onChange={(e) => setUserProfileData({ ...userProfileData, first_name: e.target.value })}
+                    autoComplete="off"
+										required
+									/>
+								</div>
+								<div className='form_group'>
+									<label htmlFor="last_name">nazwisko</label>
+									<input
+										id="last_name"
+										type="text"
+										name="last_name"
+										value={userProfileData.last_name}
+										onChange={(e) => setUserProfileData({ ...userProfileData, last_name: e.target.value })}
+                    autoComplete="off"
+										required
+									/>
+								</div>
+								<div className='form_group'>
+									<label htmlFor="email">email</label>
+									<input
+										id="email"
+										type="email"
+										name="email"
+										value={userProfileData.email}
+										onChange={(e) => setUserProfileData({ ...userProfileData, email: e.target.value })}
+                    autoComplete="off"
+										required
+									/>
+								</div>
+                <div className='form_group'>
+                  <button className="form-button" type="submit">zmień dane</button>
+                </div>
+              </form>
+
+              <form className='change-password-form' onSubmit={handleChangePassword}>
+								<div className='form_group'>
+									<label htmlFor="password">hasło</label>
+									<div className="password_image">
+										<input 
+                      id="password" 
+                      type={showNewPassword ? "text" : "password"} 
+                      name="password" 
+											value={newPassword} 
+                      onChange={(e) => setNewPassword(e.target.value)} 
+                      autoComplete="off"
+                      required
+										/>
+										<div className="eye-icon" onClick={toggleNewPasswordVisibility}>
+											{showNewPassword ? (
+												<ImEye id="eye"/> 
+											) : (
+												<ImEyeBlocked id="eye"/>
+											)}
+										</div>
+									</div>
+								</div>
+                <div className='form_group'>
+									<label htmlFor="confirm_password">powtórz hasło</label>
+									<div className="password_image">
+										<input 
+                      id="confirm_password" 
+                      type={showConfirmNewPassword ? "text" : "password"} 
+                      name="confirm_password" 
+                      value={confirmNewPassword} 
+                      onChange={(e) => setConfirmNewPassword(e.target.value)} 
+                      autoComplete="off"
+                      required
+										/>
+										<div className="eye-icon" onClick={toggleConfirmNewPasswordVisibility}>
+											{showConfirmNewPassword ? (
+												<ImEye id="eye"/> 
+											) : (
+												<ImEyeBlocked id="eye"/>
+											)}
+										</div>
+									</div>
+								</div>
+                <div className='form_group'>
+                  <button className="form-button" type="submit">zmień hasło</button>
+                </div>
+              </form>
+              
+							<form onSubmit={handleLogout} className="logout-form">
+                <div className='form_group'>
+								  <button type="submit" className="logout_button form-button">wyloguj</button>
+                </div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Settings;
