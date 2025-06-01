250f81f4719ba5c8c03e812a5787cb2c807ad969
SzymXonX <szymon.koczur5@gmail.com>
2025-06-01 19:12:35 +0200

    feat(frontend): add success and error message display to login and register pages

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import api from "../api";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate, Link} from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants"; 
 import LoadingIndicator from "../components/LoadingIndicator";
+
+import useTimedMessage from "../hooks/useTimedMessage";
 import "../styles/LoginRegister.css";
 import "../styles/Global.css";
+
 
 //Zdjęcia
 import Logo from '../assets/logo_bez_tla.png';
@@ -12,88 +16,104 @@ import { ImEye, ImEyeBlocked } from "react-icons/im";
 
 
 function Login() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [passwordVisible, setPasswordVisible] = useState(false);
-    const navigate = useNavigate();
+	const location = useLocation();
+	const state = location.state || {};
+	const message = state.message || '';
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [loading, setLoading] = useState(false);
+	const [passwordVisible, setPasswordVisible] = useState(false);
+	const navigate = useNavigate();
+	const [formError, setFormError] = useTimedMessage('');
+	const [formSuccess, setFormSuccess] = useTimedMessage(message);
 
-    const togglePasswordVisibility = () => {
-        setPasswordVisible(!passwordVisible);
-    };
-    
-    document.title = "Login";
 
-    const handleSubmit = async (e) => {
-        setLoading(true);
-        e.preventDefault();
-        try {
-            const res = await api.post("/api/token/", { username, password })
-            localStorage.setItem(ACCESS_TOKEN, res.data.access);
-            localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+	const togglePasswordVisibility = () => {
+		setPasswordVisible(!passwordVisible);
+	};
+	
+	document.title = "Login";
 
-            const userRes = await api.get("/api/user-info/", {});
-            console.log("userRes", userRes.data);
+	const handleSubmit = async (e) => {
+		setLoading(true);
+		e.preventDefault();
+		setFormError('');
+		setFormSuccess('');
+		try {
+			const res = await api.post("/api/token/", { username, password })
+			localStorage.setItem(ACCESS_TOKEN, res.data.access);
+			localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
 
-            const isSuperUser = userRes.data.is_superuser;
-            localStorage.setItem("IS_SUPERUSER", JSON.stringify(isSuperUser));
-            console.log("isSuperUser", isSuperUser);
+			const userRes = await api.get("/api/user-info/", {});
 
-            navigate("/");
-        } catch (error) {
-            alert("Błędne dane logowania. Sprawdź swoje dane i spróbuj ponownie.");
-        } finally {
-            setLoading(false);
-        }
-    };
+			const isSuperUser = userRes.data.is_superuser;
+			localStorage.setItem("IS_SUPERUSER", JSON.stringify(isSuperUser));
 
-    return (
-            <div className="container">
-                <div className="left-container">
-                    <div className="image-container">
-                        <img src={Logo} alt="Logo" />
-                    </div>
-                </div>
-                <div className="right-container">
-                    <div className="formLogin">
-                        <form className="login" onSubmit={handleSubmit}>
-                            <label htmlFor="Username">nazwa użytkownika</label>
-                            <input
-                                id="Username"
-                                type="text"
-                                name="username"
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                                required
-                            />
+			navigate("/");
+		} catch (error) {
+			setFormError("Błędne dane logowania. Sprawdź swoje dane i spróbuj ponownie.");
+		} finally {
+			setLoading(false);
+		}
+	};
 
-                            <label htmlFor="password">hasło</label>
-                            <div className="password_image">
-                                <input id="password" type={passwordVisible ? "text" : "password"} name="password" 
-                                    value={password} onChange={(e) => setPassword(e.target.value)} required
-                                    />
-                                <div className="eye-icon" onClick={togglePasswordVisibility}>
-                                    {passwordVisible ? (
-                                        <ImEye id="eye"/> 
-                                    ) : (
-                                        <ImEyeBlocked id="eye"/>
-                                    )}
-                                </div>
-                            </div>
+	return (
+		<div className="login-app-page">
+			{formError && <p className="error-message login-message">{formError}</p>}
+			{formSuccess && <p className="success-message login-message">{formSuccess}</p>}
+			<div className="container">
+				<div className="left-container">
+					<div className="image-container">
+						<img src={Logo} alt="Logo" />
+					</div>
+				</div>
+				<div className="right-container">
+					<div className="formLogin">
+						<form className="login" onSubmit={handleSubmit}>
+							<label htmlFor="Username">nazwa użytkownika</label>
+							<input
+								id="Username"
+								type="text"
+								name="username"
+								value={username}
+								onChange={(e) => setUsername(e.target.value)}
+								required
+								autoComplete="off"
+							/>
 
-                            {loading && <LoadingIndicator />}
-                            <button id="login-button" type="submit">
-                                zaloguj
-                            </button>
-                        </form>
-                        <Link id="no-account-link" to="/register">
-                            nie masz konta?
-                        </Link>
-                    </div>
-                </div>
-            </div>
+							<label htmlFor="password">hasło</label>
+							<div className="password_image">
+								<input 
+									id="password" 
+									type={passwordVisible ? "text" : "password"} 
+									name="password" 
+									value={password} 
+									onChange={(e) => setPassword(e.target.value)} 
+									required
+									autoComplete="off"
+								/>
+								<div className="eye-icon" onClick={togglePasswordVisibility}>
+									{passwordVisible ? (
+										<ImEye id="eye"/> 
+									) : (
+										<ImEyeBlocked id="eye"/>
+									)}
+								</div>
+							</div>
 
-    );
+							{loading && <LoadingIndicator />}
+							<button id="login-button" type="submit">
+								zaloguj
+							</button>
+						</form>
+						<Link id="no-account-link" to="/register">
+							nie masz konta?
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Login;
--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -19,78 +19,83 @@ function Register() {
     const [loading, setLoading] = useState(false);
     const [passwordVisible, setPasswordVisible] = useState(false);
     const navigate = useNavigate();
+    const [formError, setFormError] = useState('');
 
     const togglePasswordVisibility = () => {
         setPasswordVisible(!passwordVisible);
     };
 
-    document.title = "Rejestracja";
+    document.title = "Register";
 
     const handleSubmit = async (e) => {
         setLoading(true);
         e.preventDefault();
+        setFormError('');
         try {
             const res = await api.post("/api/user/register/", { username, password, first_name: firstName, last_name: lastName, email })
-            navigate("/login");
+            navigate("/login", { state: { message: "Rejestracja zakończona sukcesem. Teraz możesz się zalogować." } });
         } catch (error) {
-            alert("Istnieje użytkownik o podanej nazwie użytkownika");
+            setFormError("Istnieje użytkownik o podanej nazwie użytkownika");
         } finally {
             setLoading(false);
         }
     };
 
     return (
-        <div className="container">
-            <div className="left-container">
-                <div className="image-container">
-                    <img src={Logo} alt="Logo" />
+        <div className="register-app-page">
+            {formError && <p className="error-message register-message">{formError}</p>}
+            <div className="container">
+                <div className="left-container">
+                    <div className="image-container">
+                        <img src={Logo} alt="Logo" />
+                    </div>
                 </div>
-            </div>
-            <div className="right-container">
-                <div className="formLogin">
-                    <form className="login" onSubmit={handleSubmit}>
-                        <label htmlFor="firstName">imię</label>
-                        <input id="firstName" type="text" name="firstName"
-                            value={firstName} onChange={(e) => setFirstName(e.target.value)} required
-                            />
-                        
-                        <label htmlFor="lastName">nazwisko</label>
-                        <input id="lastName" type="text" name="lastName"
-                            value={lastName} onChange={(e) => setLastName(e.target.value)} required
-                            />
-
-                        <label htmlFor="email">email</label>
-                        <input id="email" type="email" name="email"
-                            value={email} onChange={(e) => setEmail(e.target.value)} required
-                            />
-
-                        <label htmlFor="Username">nazwa użytkownika</label>
-                        <input id="Username" type="text" name="username"
-                            value={username} onChange={(e) => setUsername(e.target.value)} required
-                            />
-
-                        <label htmlFor="password">hasło</label>
-                        <div className="password_image">
-                            <input id="password" type={passwordVisible ? "text" : "password"} name="password" 
-                                value={password} onChange={(e) => setPassword(e.target.value)} required
+                <div className="right-container">
+                    <div className="formLogin">
+                        <form className="login" onSubmit={handleSubmit}>
+                            <label htmlFor="firstName">imię</label>
+                            <input id="firstName" type="text" name="firstName"
+                                value={firstName} onChange={(e) => setFirstName(e.target.value)} required
                                 />
-                            <div className="eye-icon" onClick={togglePasswordVisibility}>
-                                {passwordVisible ? (
-                                    <ImEye id="eye"/> 
-                                ) : (
-                                    <ImEyeBlocked id="eye"/>
-                                )}
-                            </div>
-                        </div>
+                            
+                            <label htmlFor="lastName">nazwisko</label>
+                            <input id="lastName" type="text" name="lastName"
+                                value={lastName} onChange={(e) => setLastName(e.target.value)} required
+                                />
 
-                        {loading && <LoadingIndicator />}
-                        <button id="login-button" type="submit">
-                            zarejestruj
-                        </button>
-                    </form>
-                    <Link id="no-account-link" to="/login">
-                        masz konto?
-                    </Link>
+                            <label htmlFor="email">email</label>
+                            <input id="email" type="email" name="email"
+                                value={email} onChange={(e) => setEmail(e.target.value)} required
+                                />
+
+                            <label htmlFor="Username">nazwa użytkownika</label>
+                            <input id="Username" type="text" name="username"
+                                value={username} onChange={(e) => setUsername(e.target.value)} required
+                                />
+
+                            <label htmlFor="password">hasło</label>
+                            <div className="password_image">
+                                <input id="password" type={passwordVisible ? "text" : "password"} name="password" 
+                                    value={password} onChange={(e) => setPassword(e.target.value)} required
+                                    />
+                                <div className="eye-icon" onClick={togglePasswordVisibility}>
+                                    {passwordVisible ? (
+                                        <ImEye id="eye"/> 
+                                    ) : (
+                                        <ImEyeBlocked id="eye"/>
+                                    )}
+                                </div>
+                            </div>
+
+                            {loading && <LoadingIndicator />}
+                            <button id="login-button" type="submit">
+                                zarejestruj
+                            </button>
+                        </form>
+                        <Link id="no-account-link" to="/login">
+                            masz konto?
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
